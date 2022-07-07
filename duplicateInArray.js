ca80fdef43155d4ab651e1b961062a102c388dc4
im ranjitha <ranjithatk101@gmail.com>
2022-07-07 10:55:33 +0530

    changes

--- duplicateInArray.js
+++ duplicateInArray.js
@@ -2,6 +2,7 @@ let a = [1, 2, 3, 4, 5, 4, 3, 2, 5, 6, 9]
 let b = []
 for (let i = 0; i < a.length; i++) {
   if (b.indexOf(a[i]) === -1) {
+    //generally if no such number present returns index=-1
     b.push(a[i])
   }
 }
