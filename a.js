6dc728a807fd78e2f74ddef702fc0de530bec457
im ranjitha <ranjithatk101@gmail.com>
2022-07-31 14:02:44 +0530

    changes

--- a.js
+++ a.js
@@ -1,4 +1,5 @@
-let a = "2222"
+// count the no of 1 s
+let a = "11011010100010101001" //toString()
 function count(a) {
   let b = a.split("")
   let count = 0
@@ -10,3 +11,16 @@ function count(a) {
   return count
 }
 console.log(count(a))
+
+// let a = 00110110001
+// function count2(a) {
+//   let count = 0
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] === 1) {
+//       count++
+//     }
+//   }
+//   //   return count
+// }
+// console.log(count2(a))
+// console.log(a[i])
